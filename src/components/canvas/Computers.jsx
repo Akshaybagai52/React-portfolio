8b7ee232af3d432b9876a49a9bd3434bf51c93ab
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-04-23 18:35:03 +0530

    completed hero mobile design

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,20 +4,52 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 
-export const Computers = () => {
+export const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} scale={0.75}
-      position={[0, -3.25, -1.5]}
+      <spotLight
+      position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      shadow-mapSize={1024} />
+      <primitive 
+      object={computer.scene} 
+      scale={isMobile ? 0.7 :0.75}
+      position={isMobile ? [0, -3, -2.2] :[0, -3.25, -1.5]}
       rotation={[-0.01, -0.2, -0.1]} />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    // Set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Add the callback function as a listener for changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener("change", handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+
+
+    }
+
+
+  }, []);
   return (
     <Canvas 
       frameloop='demand'
@@ -25,12 +57,12 @@ const ComputersCanvas = () => {
       camera={{position: [20,3,5], fov:25}}
       gl={{ preserveDrawingBuffer:true}}
     >
-      <Suspense >
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2} />
-          <Computers />
+          <Computers isMobile={isMobile} />
 
       </Suspense>
       <Preload all />
