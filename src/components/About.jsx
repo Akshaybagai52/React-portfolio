a0749e6744d2420dc8e3950ce6fed17f39b10746
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-04-24 20:19:19 +0530

    connected hero section with about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({index, title, icon}) => {
   return (
@@ -18,7 +19,7 @@ const ServiceCard = ({index, title, icon}) => {
             scale: 1,
             speed: 450}}
             className='bg-tertiary rounded-[20px] py-5 px-12
-            min-h-[280px] flex justify-evenly item-center flex-col  ' >
+            min-h-[280px] flex justify-evenly items-center flex-col  ' >
           <img src={icon} alt={title}
            className='w-16 h-16 object-contain' />
            <h3 className='text-white text-[20px] font-bold
@@ -59,4 +60,4 @@ I am a problem solver and a strong communicator eager to prove my value and tale
   )
 }
 
-export default About
+export default SectionWrapper (About, "about")
