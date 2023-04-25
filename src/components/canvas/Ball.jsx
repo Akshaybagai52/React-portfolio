1c2dc2eeefc6fccd093eb2a16c33ea2223336c5a
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-04-25 22:35:35 +0530

    made structure of tech section

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
   return (
-    <div>Ball</div>
+    <Float>
+      <ambientLight intensity={2} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1,1]} />
+
+      </mesh>
+      
+    </Float> 
   )
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas 
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer:true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false} />
+          <Ball imgUrl={icon} />
+
+      </Suspense>
+      <Preload all />
+
+    </Canvas>
+  )
+}
+
+export default BallCanvas
