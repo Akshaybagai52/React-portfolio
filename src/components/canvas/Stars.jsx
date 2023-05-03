2355dc5d69415257668be94f52637ae10a210ead
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-05-03 22:41:58 +0530

    starting stars particles

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,17 +1,28 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useRef } from 'react'
 import { Canvas, useFrame} from '@react-three/fiber'
 import { Points, PointMaterial, Preload } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
 
-const Stars = () => {
+const Stars = (props) => {
+  const ref = useRef()
+
+  const sphere = random.inSphere(new Float32Array(5000), 
+   {radius: 1.2})
   return (
-    <div>Stars</div>
+    <group rotation={[0, 0, Math.PI/4]}>
+      <Points ref={ref} positions={sphere}
+        stride={3} frustumCulled {...props}>
+          <PointMaterial color="#f272c8" transparent
+            size={0.002} sizeAttenuation={true}
+            depthWrite={false} />
+        </Points>
+    </group>
   )
 }
 
 const StarsCanvas = () => {
   return (
-    <div className='w-full h-full absolute inset-0 z-[-1]'>
+    <div className='w-full h-auto absolute inset-0 z-[-1]'>
       <Canvas camera={{ position: [0, 0, 1]}}>
         <Suspense fallback={null}>
          <Stars />
