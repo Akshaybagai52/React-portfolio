ffd7aa2ef638f206d0b31b430b84c77e8394915f
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-05-03 22:13:51 +0530

    started working on earth model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,9 +65,20 @@ const Contact = () => {
              />
 
            </label>
+           <button type='submit'
+            className='bg-tertiary py-3 outline-none
+             font-bold shadow-md shadow-primary px-8 
+              w-fit text-white'>
+            {loading ? "Sending..." : "Send"}
+
+           </button>
 
         </form>
 
+      </motion.div>
+      <motion.div variants={slideIn("left", "tween", 0.2, 1)}
+       className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
+        <EarthCanvas />
       </motion.div>
 
     </div>
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return (
+    <Canvas 
+      shadows
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true}}
+      camera={{}} >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitalControls 
+            autoRotate
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+ />
+
+        </Suspense>
+
+    </Canvas>
   )
 }
 
